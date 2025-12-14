b4cd9b19b28b2628540277e8441688a4bf196d80
happiness olukayode <olukayodehappiness2006@gmail.com>
2025-12-14 19:42:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import assets from './assets/assets'
+const App = () => {
+  return (
+    <div className='bg-black'>App</div>
+  )
+}
+
+export default App
---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import iconEllipsis from "./icon-ellipsis.svg";
+import iconExercise from "./icon-exercise.svg";
+import iconPlay from "./icon-play.svg";
+import iconSelfCare from "./icon-self-care.svg";
+import iconStudy from "./icon-study.svg";
+import iconWork from "./icon-work.svg";
+
+export default {
+  iconEllipsis,
+  iconExercise,
+  iconPlay,
+  iconSelfCare,
+  iconStudy,
+  iconWork,
+};
